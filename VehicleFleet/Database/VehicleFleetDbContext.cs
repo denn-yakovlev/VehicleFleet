576cb55136bfc5f0937b020d323717b81271c2a0
Denis Yakovlev <denn.yakovlev@gmail.com>
2021-09-01 22:11:37 +0300

    Db context changes

--- VehicleFleet/Database/VehicleFleetDbContext.cs
+++ VehicleFleet/Database/VehicleFleetDbContext.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using VehicleFleet.Entities;
 
 namespace VehicleFleet.Database
 {
     public class VehicleFleetDbContext : DbContext
     {
-        
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Shift> Shifts { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connection = new NpgsqlConnection("Host=localhost;Database=vehicle_fleet;Username=postgres;Password=password");
+            optionsBuilder.UseNpgsql(connection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehicle>().Property(v => v.GearboxType).HasConversion<string>();
+        }
     }
 }
