9940f1b8215e610067090e8f1e177b04da146581
Denis Yakovlev <denn.yakovlev@gmail.com>
2021-09-01 18:24:23 +0300

    Shift entity added

---
+++ VehicleFleet/Entities/Shift.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VehicleFleet.Entities
+{
+    public class Shift
+    {
+        public DateTime Start { get; init; } = DateTime.Now.AddDays(-1.0);
+        public DateTime End { get; init; } = DateTime.Now;
+        public Vehicle Vehicle { get; }
+        public Driver Driver { get; }
+        public double Kilometrage { get; init; }
+        public double FuelConsumedLiters => Vehicle.FuelConsumptionLitersPer100Km / 100.0 * Kilometrage;
+
+        public Shift(Vehicle vehicle, Driver driver)
+        {
+            Vehicle = vehicle;
+            Driver = driver;
+        }
+    }
+}
