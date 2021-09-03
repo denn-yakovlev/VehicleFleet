481e619c2be3900f9c2fa7be9ae3cff3f4ad751a
Denis Yakovlev <denn.yakovlev@gmail.com>
2021-09-03 15:45:59 +0300

    Driver controller modified

--- VehicleFleet/Controllers/DriverController.cs
+++ VehicleFleet/Controllers/DriverController.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using VehicleFleet.Database;
 using VehicleFleet.DTO;
 using VehicleFleet.Entities;
@@ -8,7 +12,7 @@ using VehicleFleet.Entities;
 namespace VehicleFleet.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class DriverController : Controller
     {
         private readonly IMapper _mapper;
@@ -20,12 +24,51 @@ namespace VehicleFleet.Controllers
             _mapper = mapper;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             var dtos = _dbContext.Drivers.Select(ToDriverDto);
-            return Json(dtos.ToList());
+            return Json(dtos);
         }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var entity = await _dbContext.Drivers.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Drivers.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] DriverDto driverDto)
+        {
+            var entity = _mapper.Map<Driver>(driverDto);
+            _dbContext.Drivers.Add(entity);
+            await _dbContext.SaveChangesAsync();
+            return Created("api/[controller]", entity);
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, DriverDto driverDto)
+        {
+            var updatedEntity = _mapper.Map<Driver>(driverDto);
+            var entity = await _dbContext.Drivers.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Entry(entity).State = EntityState.Detached;
+            _dbContext.Drivers.Add(updatedEntity);
+            _dbContext.Entry(updatedEntity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+        
         private DriverDto ToDriverDto(Driver driver)
         {
             return _mapper.Map<DriverDto>(driver);
