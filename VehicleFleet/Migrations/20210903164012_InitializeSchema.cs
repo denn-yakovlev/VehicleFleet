c1c23b9c427a3ba936320a66b7e5b20ab6ea10c7
Denis Yakovlev <denn.yakovlev@gmail.com>
2021-09-06 00:48:28 +0300

    Db creation migration

---
+++ VehicleFleet/Migrations/20210903164012_InitializeSchema.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace VehicleFleet.Migrations
+{
+    public partial class InitializeSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drivers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FullName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drivers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Manufacturer = table.Column<string>(type: "text", nullable: true),
+                    ModelName = table.Column<string>(type: "text", nullable: true),
+                    ProductionYear = table.Column<int>(type: "integer", nullable: false),
+                    EnginePowerHp = table.Column<int>(type: "integer", nullable: false),
+                    EngineVolumeLiters = table.Column<double>(type: "double precision", nullable: false),
+                    InitialCostRoubles = table.Column<int>(type: "integer", nullable: false),
+                    GearboxType = table.Column<string>(type: "text", nullable: false),
+                    Kilometrage = table.Column<double>(type: "double precision", nullable: false),
+                    FuelConsumptionLitersPer100Km = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shifts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Start = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    End = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    VehicleId = table.Column<int>(type: "integer", nullable: true),
+                    DriverId = table.Column<int>(type: "integer", nullable: true),
+                    Kilometrage = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shifts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shifts_Drivers_DriverId",
+                        column: x => x.DriverId,
+                        principalTable: "Drivers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Shifts_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shifts_DriverId",
+                table: "Shifts",
+                column: "DriverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shifts_VehicleId",
+                table: "Shifts",
+                column: "VehicleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shifts");
+
+            migrationBuilder.DropTable(
+                name: "Drivers");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
